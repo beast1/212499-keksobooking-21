8dafa3da95e3d653f256b0b9430bc7790c5bfd63
Егор Глебов <beast.loby@gmail.com>
2020-10-07 19:40:52 +0300

    transfer legacy script to mockup.js and add pin activation, form validation script to main.js

--- js/main.js
+++ js/main.js
@@ -1,165 +1,107 @@
 'use strict';
 
-const houseDataPatterns = {
-  types: [`palace`, `flat`, `house`, `bungalow`],
-  checkin: [`12:00`, `13:00`, `14:00`],
-  features: [`wifi`, `dishwasher`, `parking`, `washer`, `elevator`, `conditioner`],
-  photos: [`http://o0.github.io/assets/images/tokyo/hotel1.jpg`, `http://o0.github.io/assets/images/tokyo/hotel2.jpg`, `http://o0.github.io/assets/images/tokyo/hotel3.jpg`]
-};
+const initPin = () => {
+  const mainPin = document.querySelector(`.map__pin--main`);
+  const activateForm = (state) => {
+    const form = document.querySelector(`.ad-form`);
+    const formFieldsetArr = form.querySelectorAll(`fieldset`);
 
-const markerOffset = {
-  X: 25,
-  Y: 70
-};
+    formFieldsetArr.forEach((formFieldset) => {
+      formFieldset.disabled = !state;
+    });
 
-const mapOverlayY = {
-  START: 130,
-  END: 630
-};
-
-const markerTextContent = {
-  TITLE: `Уютный очаг на краю города`,
-  DESCRIPTION: `Теплое место, последнее пристанище расплавленного ума`
-};
-
-const types = {
-  flat: `Квартира`,
-  bungalow: `Бунгало`,
-  house: `Дом`,
-  palace: `Дворец`
-};
-
-const generateLocation = () => {
-  const mapOverlay = document.querySelector(`.map__overlay`);
-  return {
-    x: window.randomInteger(markerOffset.X, mapOverlay.offsetWidth),
-    y: window.randomInteger(mapOverlayY.START + markerOffset.Y, mapOverlayY.END)
+    if (state) {
+      form.classList.remove(`ad-form--disabled`);
+    } else {
+      form.classList.add(`ad-form--disabled`);
+    }
   };
-};
 
-const generateFeatures = (houseRandomData) => {
-  return houseRandomData.features.filter(() => window.randomInteger(0, 1));
-};
-
-const generatePhotos = (houseRandomData) => {
-  return houseRandomData.photos.filter(() => window.randomInteger(0, 1));
-};
-
-const generateHousesData = (houseRandomData, markersCount) => {
-  const markers = [];
-
-  for (let i = 0; i < markersCount; i++) {
-    const location = generateLocation();
-
-    markers.push({
-      author: {
-        avatar: `img/avatars/user0${i + 1}.png`
-      },
-      offer: {
-        title: markerTextContent.TITLE,
-        address: `${location.x}, ${location.y}`,
-        price: window.randomInteger(999, 9999),
-        type: houseRandomData.types[window.randomInteger(0, 3)],
-        rooms: window.randomInteger(1, 5),
-        guests: window.randomInteger(1, 8),
-        checkin: houseRandomData.checkin[window.randomInteger(0, 2)],
-        checkout: houseRandomData.checkin[window.randomInteger(0, 2)],
-        features: generateFeatures(houseRandomData),
-        description: markerTextContent.DESCRIPTION,
-        photos: generatePhotos(houseRandomData)
-      },
-      location: {
-        x: location.x,
-        y: location.y
+  const setMapState = (state) => {
+    const onPinMousedown = (e) => {
+      if (e.button === 0) {
+        setMapState(true);
       }
-    });
-  }
+    };
+    const onPinKeydown = (e) => {
+      if (e.key === `Enter`) {
+        setMapState(true);
+      }
+    };
+    if (state) {
+      const map = document.querySelector(`.map`);
+      map.classList.remove(`map--faded`);
+      activateForm(true);
+      initRoomsValidate();
+      mainPin.removeEventListener(`mousedown`, onPinMousedown);
+      mainPin.removeEventListener(`keydown`, onPinKeydown);
+    } else {
+      activateForm(false);
+      mainPin.addEventListener(`mousedown`, onPinMousedown);
+      mainPin.addEventListener(`keydown`, onPinKeydown);
+    }
+  };
 
-  return markers;
+  const setAddress = () => {
+    const addressInput = document.querySelector(`#address`);
+    const coordinates = {
+      X: +mainPin.style.left.replace(/[^-0-9]/gim, ``),
+      Y: +mainPin.style.top.replace(/[^-0-9]/gim, ``)
+    };
+    addressInput.value = `${coordinates.X}, ${coordinates.Y}`;
+  };
+
+  const initRoomsValidate = () => {
+    const roomLimits = [
+      {
+        roomsCount: 1,
+        guestOptions: [{value: 1, text: `для 1 гостя`}]
+      },
+      {
+        roomsCount: 2,
+        guestOptions: [{value: 1, text: `для 1 гостя`}, {value: 2, text: `для 2 гостей`}]
+      },
+      {
+        roomsCount: 3,
+        guestOptions: [{value: 1, text: `для 1 гостя`}, {value: 2, text: `для 2 гостей`}, {
+          value: 3,
+          text: `для 3 гостей`
+        }]
+      },
+      {
+        roomsCount: 100,
+        guestOptions: [{value: 1, text: `не для гостей`}]
+      }
+    ];
+
+    const roomsSelect = document.querySelector(`#room_number`);
+    const capacitySelect = document.querySelector(`#capacity`);
+    const capacityOption = document.querySelector(`#capacity option`);
+    const roomsValidate = () => {
+      roomLimits.forEach((limit) => {
+        if (limit.roomsCount === +roomsSelect.value) {
+          const guestOptionsFragment = document.createDocumentFragment();
+          limit.guestOptions.forEach((guestOption) => {
+            const guestOptionMarkup = capacityOption.cloneNode();
+            guestOptionMarkup.innerText = guestOption.text;
+            guestOptionMarkup.value = guestOption.value;
+            guestOptionsFragment.appendChild(guestOptionMarkup);
+          });
+          capacitySelect.innerHTML = ``;
+          capacitySelect.appendChild(guestOptionsFragment);
+        }
+      });
+    };
+
+    const onRoomsSelectInput = () => {
+      roomsValidate();
+    };
+
+    roomsSelect.addEventListener(`input`, onRoomsSelectInput);
+  };
+
+  setAddress();
+  setMapState(false);
 };
 
-const showMap = () => {
-  const map = document.querySelector(`.map`);
-  map.classList.remove(`map--faded`);
-};
-
-const drawMarkers = (markersStructuredData) => {
-  const markersTemplate = document.querySelector(`#pin`);
-  const markersFragment = document.createDocumentFragment();
-  const markersParent = document.querySelector(`.map__pins`);
-
-  markersStructuredData.forEach((markerData) => {
-    const markerMarkup = markersTemplate.content.querySelector(`.map__pin`).cloneNode(true);
-    markerMarkup.style = `left: ${markerData.location.x - markerOffset.X}px; top: ${markerData.location.y - markerOffset.Y}px`;
-    markerMarkup.attributes.src = markerData.author.avatar;
-    markerMarkup.attributes.alt = markerData.offer.title;
-    markersFragment.appendChild(markerMarkup);
-  });
-
-  markersParent.appendChild(markersFragment);
-};
-
-const drawCardFeatures = (data, parent = document) => {
-  const featuresParent = parent.querySelector(`.popup__features`);
-  if (data && featuresParent) {
-    const featuresFragment = document.createDocumentFragment();
-    data.forEach((feature) => {
-      const featureMarkup = featuresParent.querySelector(`.popup__feature`).cloneNode(true);
-      featureMarkup.className = `popup__feature popup__feature--${feature}`;
-      featuresFragment.appendChild(featureMarkup);
-    });
-    featuresParent.innerHTML = ``;
-    featuresParent.appendChild(featuresFragment);
-  } else if (featuresParent) {
-    window.hideNode(featuresParent);
-  }
-};
-
-const drawCardPhotos = (data, parent = document) => {
-  const photosParent = parent.querySelector(`.popup__photos`);
-  if (data && photosParent) {
-    const photosFragment = document.createDocumentFragment();
-    data.forEach((photo) => {
-      const photoMarkup = photosParent.querySelector(`.popup__photo`).cloneNode(true);
-      photoMarkup.src = photo;
-      photosFragment.appendChild(photoMarkup);
-    });
-    photosParent.innerHTML = ``;
-    photosParent.appendChild(photosFragment);
-  } else if (photosParent) {
-    window.hideNode(photosParent);
-  }
-};
-
-const drawCardAvatar = (data, parent = document) => {
-  const avatar = parent.querySelector(`.popup__avatar`);
-  if (data && avatar) {
-    avatar.src = data;
-  } else if (avatar) {
-    window.hideNode(avatar);
-  }
-};
-
-const drawCard = (housesData) => {
-  const cardTemplate = document.querySelector(`#card`);
-  const cardMarkup = cardTemplate.content.querySelector(`.map__card`).cloneNode(true);
-  const filtersContainer = document.querySelector(`.map__filters-container`);
-
-  window.drawTextBlock(`.popup__title`, housesData.offer.title, cardMarkup);
-  window.drawTextBlock(`.popup__text--address`, housesData.offer.address, cardMarkup);
-  window.drawTextBlock(`.popup__text--price`, `${housesData.offer.price}₽/ночь`, cardMarkup);
-  window.drawTextBlock(`.popup__type`, types[housesData.offer.type], cardMarkup);
-  window.drawTextBlock(`.popup__text--capacity`, `${housesData.offer.rooms} комнаты для ${housesData.offer.guests} гостей`, cardMarkup);
-  window.drawTextBlock(`.popup__text--time`, `Заезд после ${housesData.offer.checkin}, выезд до ${housesData.offer.checkout}`, cardMarkup);
-  drawCardFeatures(housesData.offer.features, cardMarkup);
-  window.drawTextBlock(`.popup__description`, housesData.offer.description, cardMarkup);
-  drawCardPhotos(housesData.offer.photos, cardMarkup);
-  drawCardAvatar(housesData.author.avatar, cardMarkup);
-
-  filtersContainer.before(cardMarkup);
-};
-
-const housesData = generateHousesData(houseDataPatterns, 8);
-drawMarkers(housesData);
-drawCard(housesData[0]);
-showMap();
+initPin();
---
+++ js/mockup.js
@@ -0,0 +1,159 @@
+'use strict';
+
+const houseDataPatterns = {
+  types: [`palace`, `flat`, `house`, `bungalow`],
+  checkin: [`12:00`, `13:00`, `14:00`],
+  features: [`wifi`, `dishwasher`, `parking`, `washer`, `elevator`, `conditioner`],
+  photos: [`http://o0.github.io/assets/images/tokyo/hotel1.jpg`, `http://o0.github.io/assets/images/tokyo/hotel2.jpg`, `http://o0.github.io/assets/images/tokyo/hotel3.jpg`]
+};
+
+const markerOffset = {
+  X: 25,
+  Y: 70
+};
+
+const mapOverlayY = {
+  START: 130,
+  END: 630
+};
+
+const markerTextContent = {
+  TITLE: `Уютный очаг на краю города`,
+  DESCRIPTION: `Теплое место, последнее пристанище расплавленного ума`
+};
+
+const types = {
+  flat: `Квартира`,
+  bungalow: `Бунгало`,
+  house: `Дом`,
+  palace: `Дворец`
+};
+
+const generateLocation = () => {
+  const mapOverlay = document.querySelector(`.map__overlay`);
+  return {
+    x: window.randomInteger(markerOffset.X, mapOverlay.offsetWidth),
+    y: window.randomInteger(mapOverlayY.START + markerOffset.Y, mapOverlayY.END)
+  };
+};
+
+const generateFeatures = (houseRandomData) => {
+  return houseRandomData.features.filter(() => window.randomInteger(0, 1));
+};
+
+const generatePhotos = (houseRandomData) => {
+  return houseRandomData.photos.filter(() => window.randomInteger(0, 1));
+};
+
+const generateHousesData = (houseRandomData, markersCount) => {
+  const markers = [];
+
+  for (let i = 0; i < markersCount; i++) {
+    const location = generateLocation();
+
+    markers.push({
+      author: {
+        avatar: `img/avatars/user0${i + 1}.png`
+      },
+      offer: {
+        title: markerTextContent.TITLE,
+        address: `${location.x}, ${location.y}`,
+        price: window.randomInteger(999, 9999),
+        type: houseRandomData.types[window.randomInteger(0, 3)],
+        rooms: window.randomInteger(1, 5),
+        guests: window.randomInteger(1, 8),
+        checkin: houseRandomData.checkin[window.randomInteger(0, 2)],
+        checkout: houseRandomData.checkin[window.randomInteger(0, 2)],
+        features: generateFeatures(houseRandomData),
+        description: markerTextContent.DESCRIPTION,
+        photos: generatePhotos(houseRandomData)
+      },
+      location: {
+        x: location.x,
+        y: location.y
+      }
+    });
+  }
+
+  return markers;
+};
+
+const drawMarkers = (markersStructuredData) => {
+  const markersTemplate = document.querySelector(`#pin`);
+  const markersFragment = document.createDocumentFragment();
+  const markersParent = document.querySelector(`.map__pins`);
+
+  markersStructuredData.forEach((markerData) => {
+    const markerMarkup = markersTemplate.content.querySelector(`.map__pin`).cloneNode(true);
+    markerMarkup.style = `left: ${markerData.location.x - markerOffset.X}px; top: ${markerData.location.y - markerOffset.Y}px`;
+    markerMarkup.attributes.src = markerData.author.avatar;
+    markerMarkup.attributes.alt = markerData.offer.title;
+    markersFragment.appendChild(markerMarkup);
+  });
+
+  markersParent.appendChild(markersFragment);
+};
+
+const drawCardFeatures = (data, parent = document) => {
+  const featuresParent = parent.querySelector(`.popup__features`);
+  if (data && featuresParent) {
+    const featuresFragment = document.createDocumentFragment();
+    data.forEach((feature) => {
+      const featureMarkup = featuresParent.querySelector(`.popup__feature`).cloneNode(true);
+      featureMarkup.className = `popup__feature popup__feature--${feature}`;
+      featuresFragment.appendChild(featureMarkup);
+    });
+    featuresParent.innerHTML = ``;
+    featuresParent.appendChild(featuresFragment);
+  } else if (featuresParent) {
+    window.hideNode(featuresParent);
+  }
+};
+
+const drawCardPhotos = (data, parent = document) => {
+  const photosParent = parent.querySelector(`.popup__photos`);
+  if (data && photosParent) {
+    const photosFragment = document.createDocumentFragment();
+    data.forEach((photo) => {
+      const photoMarkup = photosParent.querySelector(`.popup__photo`).cloneNode(true);
+      photoMarkup.src = photo;
+      photosFragment.appendChild(photoMarkup);
+    });
+    photosParent.innerHTML = ``;
+    photosParent.appendChild(photosFragment);
+  } else if (photosParent) {
+    window.hideNode(photosParent);
+  }
+};
+
+const drawCardAvatar = (data, parent = document) => {
+  const avatar = parent.querySelector(`.popup__avatar`);
+  if (data && avatar) {
+    avatar.src = data;
+  } else if (avatar) {
+    window.hideNode(avatar);
+  }
+};
+
+const drawCard = (housesData) => {
+  const cardTemplate = document.querySelector(`#card`);
+  const cardMarkup = cardTemplate.content.querySelector(`.map__card`).cloneNode(true);
+  const filtersContainer = document.querySelector(`.map__filters-container`);
+
+  window.drawTextBlock(`.popup__title`, housesData.offer.title, cardMarkup);
+  window.drawTextBlock(`.popup__text--address`, housesData.offer.address, cardMarkup);
+  window.drawTextBlock(`.popup__text--price`, `${housesData.offer.price}₽/ночь`, cardMarkup);
+  window.drawTextBlock(`.popup__type`, types[housesData.offer.type], cardMarkup);
+  window.drawTextBlock(`.popup__text--capacity`, `${housesData.offer.rooms} комнаты для ${housesData.offer.guests} гостей`, cardMarkup);
+  window.drawTextBlock(`.popup__text--time`, `Заезд после ${housesData.offer.checkin}, выезд до ${housesData.offer.checkout}`, cardMarkup);
+  drawCardFeatures(housesData.offer.features, cardMarkup);
+  window.drawTextBlock(`.popup__description`, housesData.offer.description, cardMarkup);
+  drawCardPhotos(housesData.offer.photos, cardMarkup);
+  drawCardAvatar(housesData.author.avatar, cardMarkup);
+
+  filtersContainer.before(cardMarkup);
+};
+
+const housesData = generateHousesData(houseDataPatterns, 8);
+drawMarkers(housesData);
+drawCard(housesData[0]);
