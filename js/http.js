72c4c77233a7451a259bb2ddee2dc3ebf57f7bc9
Егор Глебов <beast.loby@gmail.com>
2020-11-03 21:50:33 +0200

    finish refactor and upd bundle.js

--- js/http.js
+++ js/http.js
@@ -1,54 +1,57 @@
 'use strict';
 
-(() => {
-  const http = (requestType, url, onSuccess, onError, data) => {
-    const xhr = new XMLHttpRequest();
-    xhr.responseType = `json`;
-    let method;
 
-    if (requestType === `load`) {
-      method = `GET`;
-      xhr.timeout = 10000;
+const requestTypes = {
+  LOAD: `load`,
+  UPLOAD: `upload`
+};
+const http = (requestType, url, onSuccess, onError, data) => {
+  const xhr = new XMLHttpRequest();
+  xhr.responseType = `json`;
+  let method;
 
-      xhr.addEventListener(`error`, () => {
-        onError(window.data.messages.load.common);
-      });
-      xhr.addEventListener(`timeout`, () => {
-        onError(`${window.data.messages.load.timeout.pre} ${xhr.timeout} ${window.data.messages.load.timeout.post}`);
-      });
-    } else if (requestType === `upload`) {
-      method = `POST`;
-    }
+  if (requestType === requestTypes.LOAD) {
+    method = `GET`;
+    xhr.timeout = 10000;
 
-    xhr.addEventListener(`load`, () => {
-      let err;
-      switch (xhr.status) {
-        case window.data.statusDictionary.success:
-          if (requestType === `load`) {
-            onSuccess(xhr.response);
-          } else if (requestType === `upload`) {
-            onSuccess(window.data.messages.upload.success);
-          }
-          break;
-        case window.data.statusDictionary.badRequest:
-          err = window.data.messages.upload.err400;
-          break;
-        case window.data.statusDictionary.missingAuth:
-          err = window.data.messages.upload.err401;
-          break;
-        case window.data.statusDictionary.notFound:
-          err = window.data.messages.upload.err404;
-          break;
-        default:
-          err = `${window.data.messages.upload.status} ${xhr.status} ${xhr.statusText}`;
-      }
-      if (err) {
-        onError(`${xhr.status} ${xhr.statusText}`);
-      }
+    xhr.addEventListener(`error`, () => {
+      onError(window.data.messages.http.errDefault);
     });
+    xhr.addEventListener(`timeout`, () => {
+      onError(`${window.data.messages.http.timeout.pre} ${xhr.timeout} ${window.data.messages.http.timeout.post}`);
+    });
+  } else if (requestType === requestTypes.UPLOAD) {
+    method = `POST`;
+  }
 
-    xhr.open(method, url);
-    xhr.send(data);
-  };
-  window.http = http;
-})();
+  xhr.addEventListener(`load`, () => {
+    let err;
+    switch (xhr.status) {
+      case window.data.statusDictionary.success:
+        if (requestType === requestTypes.LOAD) {
+          onSuccess(xhr.response);
+        } else if (requestType === requestTypes.UPLOAD) {
+          onSuccess(window.data.messages.http.success);
+        }
+        break;
+      case window.data.statusDictionary.badRequest:
+        err = window.data.messages.http.err400;
+        break;
+      case window.data.statusDictionary.missingAuth:
+        err = window.data.messages.http.err401;
+        break;
+      case window.data.statusDictionary.notFound:
+        err = window.data.messages.http.err404;
+        break;
+      default:
+        err = `${window.data.messages.http.status} ${xhr.status} ${xhr.statusText}`;
+    }
+    if (err) {
+      onError(`${xhr.status} ${xhr.statusText}`);
+    }
+  });
+
+  xhr.open(method, url);
+  xhr.send(data);
+};
+window.http = http;
